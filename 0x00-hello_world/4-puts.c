4a6fa3a6f5118dcfac92414b32bd1ce3bb53b824
esru13 <esraelb313@gmail.comgit config --global user.name esru13>
2022-09-08 13:27:36 -0700

    put

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stido.h>
+
+/**
+*main - print the string in the put function 
+
+[C[D
+
+*
+* Description :using the main function
+*this program prints "programming is like building a multilingual puzzle
+*Return: 0;
+int main(void)
+{
+	puts ("\"programming is like building a mutilingual puzzle");
+	return (0);
+}
