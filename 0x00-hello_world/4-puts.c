39e56bd1357a6e4ac197cb04123de2edc879fb59
esru13 <esraelb313@gmail.comgit config --global user.name esru13>
2022-09-09 16:10:27 -0700

    put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,12 +3,16 @@
 /**
 * main - print the string in the put function
 *
-* Description: using the mainfunction
+* Description: using the main function
 * this program prints "programming is like building a multilingual puzzle
 * Return: 0
 */
 int main(void)
+
 {
-	puts("\"programming is like building a multilingual puzzle");
-	return (0);
+
+    puts("\programming is line building  a multilingual puzzle");
+
+    return(0);
+
 }
