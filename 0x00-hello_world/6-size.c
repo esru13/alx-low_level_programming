bfb622ac260569765da654a78c56cf3c7603e6d7
esru13 <esraelb313@gmail.comgit config --global user.name esru13>
2022-09-11 15:42:13 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,24 +1,45 @@
-#include <studio.h>
+#include <stdio.h>
 
 /**
-* main - print the string in the put function 
-*
-* Description: using the main function
-* this program prints "programming is like building a multilingual puzzle
-* Return: 0
-*/
-int main(void)
-{
-	char c;
-	int i;
-	long li;
-	long long lli;
-	float f;
 
-	printf("size of char: %ld byte(s)\n", sizeof(c));
-	printf("size of an int: %ld byte(s)\n", sizeof(i));
-	printf("size of a long int: %ld byte(s)\n", sizeof(li));
-	printf("size of a long long int: %ld byte(s)\n", sizeof(lli));
-	printf("size of a float: %ld byte(s)\n", sizeof(f));
-	return (0);
+ * main - print the string in the put function
+
+ * 
+
+ * Description: using the main function
+
+ * this program prints "programming is is like building a multilignual puzzle
+
+ * Return: 0
+
+ */
+
+int main(void)
+
+{
+
+    char c;
+
+    int i;
+
+    long li;
+
+    long long lli;
+
+    float f;
+
+
+
+    printf("Size of a char: %ld byte(s)\n", sizeof(c));
+
+    printf("Size of an int: %ld byte(s)\n", sizeof(i));
+
+    printf("Size of a long int: %ld byte(s)\n", sizeof(li));
+
+    printf("Size of a long long: %ld byte(s)\n", sizeof(lli));
+
+    printf("Size of a float: %ld byte(s)\n", sizeof(f));
+
+    return (0);
+
 }
