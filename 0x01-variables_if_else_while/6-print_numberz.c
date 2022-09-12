e0f755fd78bc6318a7c03781490f8fe9927f114e
esru13 <esraelb313@gmail.comgit config --global user.name esru13>
2022-09-12 16:57:37 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+
+ * main - prints the number since 0 to 9
+
+ * 
+
+ * Return: Always(Success)
+
+ */
+
+int main(void){
+
+    int n;
+
+    for(n='0';n<='9';n++){
+
+        putchar(n);
+
+    }
+
+    putchar('\n');
+
+    return (0);
+
+}
