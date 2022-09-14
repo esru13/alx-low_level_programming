0ff0e450f237ec508d8043715700a182bfd46790
esru13 <esraelb313@gmail.comgit config --global user.name esru13>
2022-09-13 18:03:05 -0700

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
-* print_alphabet - function to print abc
-*
-* Return: 0
-*/
+ * print_alphabet - function to print abc
+ *
+ * Return: 0
+ */
 void print_alphabet(void)
 {
 	char c;
+
 	for (c = 'a'; c <= 'z'; c++)
 	{
 		_putchar(c);
