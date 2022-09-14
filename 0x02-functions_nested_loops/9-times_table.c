7fae4499285545da2148f571caeeecdb23070a67
esru13 <esraelb313@gmail.comgit config --global user.name esru13>
2022-09-13 17:44:59 -0700

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,10 +1,12 @@
 #include "main.h"
+
 /**
  *times_table - Prints the 9 times table, starting with 0.
  */
 void times_table(void)
 {
 	int num, mult, prod;
+
 	for (num = 0; num <= 9; num++)
 	{
 		_putchar('0');
