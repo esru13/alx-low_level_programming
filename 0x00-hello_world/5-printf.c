6cc47f96194f567eb973e806a06e44be55d624d1
esru13 <esraelb313@gmail.com>
2022-11-08 15:41:09 +0300

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,25 +1,15 @@
 
-
 #include <stdio.h>
 
 /**
-
  * main - print the string in the put function
-
  * 
-
  * Description: using the main function
-
  * this program prints "programming is is like building a multilignual puzzle"
-
  * Return: 0
-
  */
-
-int main(void){
-
+int main(void)
+{
     printf("with proper grammar, but the outcome is a piece of art,\n");
-
     return (0);
-
 }
