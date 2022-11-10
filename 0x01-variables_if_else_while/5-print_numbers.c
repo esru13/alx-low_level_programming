b3dddbc58c1359b168a44a9ed4ccd96c8c85bcbd
esru13 <esraelb313@gmail.com>
2022-11-10 14:04:37 +0300

    print

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -3,7 +3,6 @@
 /**
  * main - print if the number is  positive , zero or negative 
  * Description: using the main function
- *
  * this program prints "programming is positive , zero or negative"
  * Return: 0
  */
