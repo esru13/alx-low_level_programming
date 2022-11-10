9cfb5b069222c4e4082a0a5760d6dafb01421d82
esru13 <esraelb313@gmail.com>
2022-11-10 14:03:09 +0300

    pprint numb

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 
 /**
- * main - print if the number is  positive , zero or negative
- * 
+ * main - print if the number is  positive , zero or negative 
  * Description: using the main function
+ *
  * this program prints "programming is positive , zero or negative"
  * Return: 0
  */
 
-int main(void){
+int main(void)
+{
     int i;
 
     for (i=0 ; i<10 ; i++){
