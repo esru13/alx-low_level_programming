0e2a31236f8aca9b96d1559f2091f65058b1eaf5
esru13 <esraelb313@gmail.com>
2022-11-15 10:20:36 +0300

    print comb

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,55 +1,31 @@
 #include <stdio.h>
 
 /**
-
- * main - prints single digit numbers starting from 0
-
- * 
-
- * Return: returns zero at the end
-
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
+int main(void)
+{
+	int num1, num2;
 
-int main(void){
-
-    int i,j;
-
-
-
-    for(i=0;i<=9;i++){
-
-
-
-        for(j=1;j<=9;j++){
-
-
-
-            if(j>i)
-
-            {
-
-                putchar(i+'0');
-
-                putchar(j+'0');
-
-                if(i!=8)
-
-                {
-
-                    putchar(',');
-
-                    putchar(' ');
-
-                }
-
-            } 
-
-        }
-
-    }
-
-    putchar('\n');
-
-    return (0);
-
+	for (num1 = '0'; num1 < '9'; num1++)
+	{
+		for (num2 = num1 + 1; num2 <= '9'; num2++)
+		{
+			if (num1 != num2)
+			{
+				putchar(num1);
+				putchar(num2);
+				if (num1 == '8' && num2 == '9')
+				{
+					continue;
+				}
+			}
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
 }
