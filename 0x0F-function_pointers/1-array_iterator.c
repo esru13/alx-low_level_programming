f16db848942ee6c7d7b96156717b414925a6d8e8
esru13 <esraelb313@gmail.com>
2022-11-15 10:56:25 +0300

    array iteration

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,27 +1,22 @@
-#include "function_pointers.h"
+#include <stdio.h>
 
 /**
- * int_index - searches for an integer
- * @array: array to serch through
- * @size: size of array
- * @cmp: function used to compare
- *
- * Return: first index for which cmp doesn't return 0, or -1
+ * array_iterator - executes funct
+ * @array: array of elements
+ * @size: array's size
+ * @action: pointer to function
+ * Return: No
  */
-int int_index(int *array, int size, int (*cmp)(int))
-{
-	int i, r;
 
-	if (size > 0 && array && cmp)
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (array && size && action)
 	{
 		for (i = 0; i < size; i++)
 		{
-			r = cmp(array[i]);
-			if (r)
-				break;
+			(*action)(array[i]);
 		}
-		if (i < size)
-			return (i);
 	}
-	return (-1);
 }
