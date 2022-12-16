2f69f56ab4538c988fb3597e75e55c195651be0e
esru13 <esraelb313@gmail.com>
2022-12-17 00:48:59 +0300

    dynamic

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gcc -fPIC -c *.c
+gcc -c *.c -fpic
 gcc *.o -shared -o liball.so
--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,15 +1,19 @@
 #ifndef MAIN_H
 #define MAIN_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
 int _abs(int n);
+int _isupper(int c);
 int _isdigit(int c);
-int _isupper(int c;
 int _strlen(char *s);
-void _puts(char *str);
-int _atoi(char *s);
+void _puts(char *s);
 char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
